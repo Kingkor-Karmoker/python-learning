ebbe0344e16d4ab27b722e4fde0415d57ed01a93
Kingkor-Karmoker <99kingslash@gamil.com>
2026-06-09 00:38:54 +0600

    chapter 2 prob 2.7

---
+++ CH2_Prb_2-7.py
@@ -0,0 +1,9 @@
+person_name = ' Kingkor '
+print('\t'+person_name)
+print('\n'+person_name)
+print(person_name)
+
+#now printing persons name but stripping the whitespace
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name.strip())
