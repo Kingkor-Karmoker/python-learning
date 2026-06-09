d8623dc0848f4cd564ef108b09efa74cbc08c0ea
Kingkor-Karmoker <99kingslash@gamil.com>
2026-06-09 18:44:16 +0600

    Chapter 3 problem 3.3

---
+++ CH3_Prb_3-3.py
@@ -0,0 +1,4 @@
+favourite_transportations = ['Car', 'launch', 'Plane']
+for transportation in favourite_transportations:
+    print(f"My favourite transportation is: {transportation}")
+    print(f"I would like to have a {transportation} of my own one day")
