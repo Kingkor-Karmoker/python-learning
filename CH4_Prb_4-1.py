b3f4d9126bea0fd69a56a5525b01a2362da208d5
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-10 20:36:07 +0600

    Chapter 4 problem 4.1

---
+++ CH4_Prb_4-1.py
@@ -0,0 +1,11 @@
+#Chapter 3 problem 4-1:
+
+favourite_Pizzas = ['Margherita', 'Pepperoni', 'Meat Lover', 'BBQ Chicken']
+
+for pizza in favourite_Pizzas:
+    print(pizza)
+#modifying for loop:
+
+for pizza in favourite_Pizzas:
+    print(f"I love eating {pizza} pizza so much")
+print("I really love to eat pizza's")
