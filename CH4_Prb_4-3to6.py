c29ad232b79138bd591e3e1acce18b01798b82b1
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-11 02:14:13 +0600

    Chapter 4 problem 3to6

---
+++ CH4_Prb_4-3to6.py
@@ -0,0 +1,14 @@
+#code for chapter 4 3to6:
+for value in range(1,21):
+    print(value)
+
+values = list(range(1,1000001))
+for val in values:
+    print(val)
+
+print(max(values))
+print(min(values))
+print(sum(values))
+
+for odd in range(1,20,2):
+    print(odd)
