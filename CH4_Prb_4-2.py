fb66ac1b909625f048250ddfaae1e39760f702a6
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-10 21:43:30 +0600

    Chapter 4 problem 4.2

---
+++ CH4_Prb_4-2.py
@@ -0,0 +1,12 @@
+animals = []
+animals.append('Dog')
+animals.append('Cat')
+animals.append('Cow')
+animals.append('Bird')
+
+print(animals)
+
+for animal in animals:
+    print(f"A {animal} would make a great pet")
+
+print("all of them will be a great pet")
