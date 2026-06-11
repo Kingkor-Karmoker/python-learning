67aa9548da468a676b26f94129c1c355c7b908b8
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-11 22:33:32 +0600

    Chapter 4 problem 4.10

---
+++ CH4_Prb_4-10.py
@@ -0,0 +1,21 @@
+#Code of chapter 4 problem 10:
+lists = [1,2,3,4,5,6,7,8,9]
+print(f"first value from first three item: {lists[0]}")
+print(f"2nd value from first three item: {lists[1]}")
+print(f"3rd value from first three item: {lists[2]}")
+
+print(f"printing first 3 items from list: {lists[0:3]}")
+
+#now for middle 3 items:
+print(f"first value from middle three item: {lists[3]}")
+print(f"2nd value from middle three item: {lists[4]}")
+print(f"3rd value from middle three item: {lists[5]}")
+
+print(f"printing middle 3 items from list: {lists[3:6]}")
+
+#now for last 3 items:
+print(f"first value from last three item: {lists[-3]}")
+print(f"2nd value from last three item: {lists[-2]}")
+print(f"3rd value from last three item: {lists[-1]}")
+
+print(f"printing last 3 items from list: {lists[-3:]}")
