78fde5547a19a428412530758cfa3668c042c5d8
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-12 17:23:08 +0600

    Chapter 5 problem 5.1

---
+++ CH4_Prb_5-1.py
@@ -0,0 +1,36 @@
+# code for chapter 5 problem 1:
+team = 'Portugal'
+food = 'Burger'
+place = 'Poland'
+supplement = 'Creatine'
+gym = 'gym'
+
+print("Is team = Portugal? I think its true")
+print(team == 'Portugal')
+
+print("Is team = Brazil? I think its false")
+print(team == 'Brazil')
+
+print("Is food = Burger? I think its true")
+print(food == 'Burger')
+
+print("Is Food = pizza? I think its false")
+print(food == 'Pizza')
+
+print("Is place = Poland? I think its true")
+print(place == 'Poland')
+
+print("Is place = India? I think its false")
+print(place == 'India')
+
+print("Is Supplement = creatine? I think its true")
+print(supplement == 'Creatine')
+
+print("Is supplement = Whey? I think its false")
+print(supplement == 'Whey')
+
+print("Is gym = gym? I think its true")
+print(gym.title() == 'Gym')
+
+print("Is gym = Home? I think its false")
+print(gym == 'Home')
