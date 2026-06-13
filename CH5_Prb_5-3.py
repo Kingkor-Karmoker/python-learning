8f716738b6d3c2b70a7bd4ce599c5814655c72a1
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-13 21:11:03 +0600

    Chapter 5 problem 5.3

---
+++ CH5_Prb_5-3.py
@@ -0,0 +1,11 @@
+# code for problem 5.3:
+color = 'Green'
+
+if color == 'Green':
+    print("You earned 5 points")
+
+# now failed test:
+color = 'Red'
+
+if color == 'Green':
+    print("You earned 5 points")
