ec3f5ad7dbff5e3c812dc5192a163a3ecfbc58bf
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-13 21:24:46 +0600

    Chapter 5 problem 4

---
+++ CH5_Prb_5-4.py
@@ -0,0 +1,13 @@
+# code for problem 5.4:
+color = 'Green'
+if color == 'Green':
+    print("you earned 5 points")
+else:
+    print("you earned 10 points")
+
+# for else version:
+color = 'Red'
+if color == 'Green':
+    print("you earned 5 points")
+else:
+    print("you earned 10 points")
