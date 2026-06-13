94d1ecb592e23fabbb0d3b4cd8f7d6ad7769b346
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-13 22:47:58 +0600

    Chapter 5 problem 5.7

---
+++ CH5_Prb_5-7.py
@@ -0,0 +1,15 @@
+# code for problem 5.7:
+favourite_fruits = ['Mango', 'Litchi', 'Jackfruit', 'Orange']
+
+if 'Kiwi' in favourite_fruits:
+    print("You really love Kiwi")
+if 'Rambutan' in favourite_fruits:
+    print("You really love Rambutan")
+if 'Coconut' in favourite_fruits:
+    print("You really love Coconut")
+if 'Litchi' in favourite_fruits:
+    print("You really love Litchi")
+if 'Strawberry' in favourite_fruits:
+    print("You really love Strawberry")
+if 'Banana' in favourite_fruits:
+    print("You really love Banana")
