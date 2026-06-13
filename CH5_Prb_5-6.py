74a3859a33296b7b4e08f17212f3240eb6ba35ce
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-13 22:33:51 +0600

    Chapter 5 Problem 5.6

---
+++ CH5_Prb_5-6.py
@@ -0,0 +1,15 @@
+# code for problem 5.6:
+age = 44
+
+if age < 2:
+    print("The person is a baby")
+elif 2 <= age < 4:
+    print("The person is a toddler")
+elif 4 <= age < 13:
+    print("The person is a kid")
+elif 13 <= age < 20:
+    print("The person is a teenager")
+elif 20 <= age < 65:
+    print("The person is an adult")
+else:
+    print("The person is an elder")
