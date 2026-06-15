3d754b309ae57eb712e4a492897ed37812efb039
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-15 23:47:52 +0600

    Chapter 5 problem 5.11

---
+++ CH5_Prb_5-11.py
@@ -0,0 +1,23 @@
+# code for problem 5.11:
+numbers = list(range(1,10))
+print(numbers)
+
+for i in numbers:
+    if i == 1:
+        print(f"{i}st")
+    elif i == 2:
+        print(f"{i}nd")
+    elif i == 3:
+        print(f"{i}rd")
+    elif i == 4:
+        print(f"{i}th")
+    elif i == 5:
+        print(f"{i}th")
+    elif i == 6:
+        print(f"{i}th")
+    elif i == 7:
+        print(f"{i}th")
+    elif i == 8:
+        print(f"{i}th")
+    else:
+        print(f"{i}th")
