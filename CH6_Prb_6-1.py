ab847a6e9fd01f68b32300fd15c80317fb07ee38
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-16 22:43:08 +0600

    Chapter 6 problem 6.1

---
+++ CH6_Prb_6-1.py
@@ -0,0 +1,11 @@
+# code for problem 6.1:
+person = {
+    'First_name': 'Tahsin',
+    'Last_name': 'Deep',
+    'Age': 24,
+    'City': 'Dhaka'
+}
+print(person['First_name'])
+print(person['Last_name'])
+print(person['Age'])
+print(person['City'])
