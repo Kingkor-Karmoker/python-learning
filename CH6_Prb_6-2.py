0acae8fa5dc74510cafa2e7ce885e4fc786a0831
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-17 00:30:52 +0600

    Chapter 6 problem 6.2

---
+++ CH6_Prb_6-2.py
@@ -0,0 +1,14 @@
+# code for problem 6.2:
+numbers = {
+    'Kingkor': 99,
+    'Deep': 69,
+    'Wasik': 62,
+    'Shaily': 9,
+    'Shakib': 19
+}
+
+print(f"Kingkor's favourite number is: {numbers['Kingkor']}")
+print(f"Deep's favourite number is: {numbers['Deep']}")
+print(f"Wasik's favourite number is: {numbers['Wasik']}")
+print(f"Shakib's favourite number is: {numbers['Shakib']}")
+print(f"Shaily's favourite number is: {numbers['Shaily']}")
