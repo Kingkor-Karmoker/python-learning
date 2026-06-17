81a9ca73baa5044a6e73917ba810addb32ee7e9c
Kingkor-Karmoker <99kingslash@gmail.com>
2026-06-17 19:44:15 +0600

    Chapter 6 problem 6.3

---
+++ CH6_Prb_6-3.py
@@ -0,0 +1,20 @@
+# code for problem 6.3:
+programing_words = {
+    'String': "a fundamental data type that represents a sequence of characters ",
+    'Int': "Integers data type",
+    'Float': "Data types for floating numbers",
+    'Append': "Assigning new values at the end of a list",
+    'List': "A data structure to store values"
+}
+
+word1 = 'String'
+word2 = 'Int'
+word3 = 'Float'
+word4 = 'Append'
+word5 = 'List'
+
+print(f"\n{programing_words['String'].rstrip()} :{word1}")
+print(f"\n{programing_words['Int']} :{word2}")
+print(f"\n{programing_words['Float']} :{word3}")
+print(f"\n{programing_words['Append']} :{word4}")
+print(f"\n{programing_words['List']} :{word5}")
