1468caff818143a8cd674205bf3df77bce78f7f1
Kingkor-Karmoker <99kingslash@gmail.com>
2026-07-09 22:33:16 +0600

    Chapter 8 problem 8.5

---
+++ CH8_Prb_8-5.py
@@ -0,0 +1,7 @@
+# code for problem 8.5:
+def describe_city(city_name, country_name = 'Bangladesh'):
+    print(f"{city_name} is in {country_name}\n")
+
+describe_city('Khulna')
+describe_city('Barisal')
+describe_city(city_name= 'Mumbai', country_name= 'India')
