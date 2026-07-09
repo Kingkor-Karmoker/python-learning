bd992ca32f38fffc9fa501fc90d65c47c76673fe
Kingkor-Karmoker <99kingslash@gmail.com>
2026-07-09 22:25:22 +0600

    Chapter 8 problem 8.4

---
+++ CH8_Prb_8-4.py
@@ -0,0 +1,10 @@
+# code for problem 8.4:
+# modifying code from 8.3:
+# code for problem 8.3:
+def make_shirt(size='Large', message='I love python'):
+    print(f"I am making a {size} shirt")
+    print(f"Message to future self: '{message}'\n")
+
+make_shirt()
+make_shirt(size= 'medium')
+make_shirt(size= 'small', message="I want to be a programmer")
