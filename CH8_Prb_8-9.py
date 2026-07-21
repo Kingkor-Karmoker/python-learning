58bc3f1d08631c3d852f52e8fd72274e8bc68f60
Kingkor-Karmoker <99kingslash@gmail.com>
2026-07-22 02:01:20 +0600

    Chapter 8 problem 8.9

---
+++ CH8_Prb_8-9.py
@@ -0,0 +1,8 @@
+# code for problem 8.9:
+def show_messages(messages):
+    for message in messages:
+        print(message)
+        print('\n')
+
+messages = ["Life is not easy", "Dont believe anyone 100%", "Win yourself first before winning others"]
+show_messages(messages)
