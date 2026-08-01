f45e27b86eaf05e92f859e0683ab2a4a411aca1f
Kingkor-Karmoker <99kingslash@gmail.com>
2026-08-01 17:22:54 +0600

    Chapter 8 problem 8.15 function module

---
+++ CH8_Prb_8_15_Printing_functions.py
@@ -0,0 +1,5 @@
+# code for problem 8.15:
+# Creating this file to reuse in another file or module:
+def printing_function(messages):
+    for msg in messages:
+        print(msg)
---
+++ CH8_Prb_8_15_Printing_models.py
@@ -0,0 +1,6 @@
+# 2nd part of code for problem 8.15:
+#importing the module:
+import CH8_Prb_8_15_Printing_functions as pf
+
+message = ["I am ok", "I miss you", "I hope you are happy"]
+pf.printing_function(message)
