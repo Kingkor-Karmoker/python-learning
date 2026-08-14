0cbded27a6729b60c0f22ff230c4cafa2b8d6532
Kingkor-Karmoker <99kingslash@gmail.com>
2026-08-14 19:12:32 +0600

    Chapter 9 problem 9.10(importing class)

---
+++ CH9_Prb_9_10_import.py
@@ -0,0 +1,6 @@
+#code for problem 9.10(import):
+import CH9_Prb_9_10_module
+
+restaurant = CH9_Prb_9_10_module.Restaurant("Abesh", "Bangladeshi food")
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
