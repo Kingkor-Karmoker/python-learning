8de300029aaebcd1507684054302ac0ba09a6180
Kingkor-Karmoker <99kingslash@gmail.com>
2026-08-16 21:03:11 +0600

    Chapter 9 problem 9.12 import

---
+++ CH9_Prb_9_12_import.py
@@ -0,0 +1,5 @@
+# code for problem 9.12(import):
+from CH9_Prb_9_12_module2 import Admin
+admin = Admin('Kingkor', 'Karmoker', '28-06-2003', 'Student')
+admin.privileges.privileges = ["I hope it was like before", "I just want be chosen", "I want to be loved"]
+admin.privileges.show_privileges()
